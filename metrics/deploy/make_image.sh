cfa650c2e29025b534006054a5a06a9c60485c05
btian <ben.tian@graviti.cn>
2020-10-10 16:15:06 +0800

    build script for making metrics docker image

---
+++ metrics/deploy/make_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [[ $#<1 ]]; then
+    ITEM="metrics"
+    COMMIT_ID=$(git log --oneline -1 | awk '{print $1}')
+    DATE_TIME=$(date +%Y%m%d)
+    IMAGE_NAME="hub.graviti.cn/prediction-challenge/${ITEM}"
+
+    echo "start build ${ITEM}..."
+    IMG_TAG=${DATE_TIME}-${COMMIT_ID}
+    IMAGE=${IMAGE_NAME}:${IMG_TAG}
+    sed -i "/image: \S*${ITEM}\:*/c\    image: ${IMAGE}" ./docker-compose.yaml
+
+else
+    IMAGE=$1
+fi
+
+cd ..
+docker build -f deploy/Dockerfile -t ${IMAGE} .
+
+
+echo build complete, to push the result, run:
+echo sudo docker push ${IMAGE}
