f58a8c65ddb184939a591203283e80f5bfb11345
btian <ben.tian@graviti.cn>
2020-10-12 17:13:53 +0800

    upload all metrics result as well as score to remote server

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -6,20 +6,31 @@ import logging
 from utils import dataset_reader
 from utils import metrics_calculator
 
-VeryLargeErrorValue = 10e6
+VeryLargeErrorValue = 10e9
 failed_metric = {
-    'jerk': VeryLargeErrorValue,
-    'velo': VeryLargeErrorValue,
-    'yaw2lane': VeryLargeErrorValue,
-    'collision': VeryLargeErrorValue,
-    'duration': VeryLargeErrorValue
+    'score': -VeryLargeErrorValue
 }
 
 
 def do_metric(logger, simulation_log_files: metrics_utils.ScenarioLogFiles) -> {}:
-    log_file = simulation_log_files.log_file.path if simulation_log_files.log_file is not None else ''
-    collision_file = simulation_log_files.collision_file.path if simulation_log_files.collision_file is not None else ''
-    config_file = simulation_log_files.config_file.path if simulation_log_files.config_file is not None else ''
+    if simulation_log_files.log_file:
+        log_file = simulation_log_files.log_file.path
+    else:
+        logger.error('no log file to calculate metrics')
+        return failed_metric
+
+    if simulation_log_files.collision_file:
+        collision_file = simulation_log_files.collision_file.path
+    else:
+        logger.error('no collision file to calculate metrics')
+        return failed_metric
+
+    if simulation_log_files.config_file:
+        config_file = simulation_log_files.config_file.path
+    else:
+        logger.error('no config file to calculate metrics')
+        return failed_metric
+
     if not os.path.exists(log_file) or len(collision_file) == 0 or len(config_file) == 0:
         return failed_metric
 
@@ -27,30 +38,25 @@ def do_metric(logger, simulation_log_files: metrics_utils.ScenarioLogFiles) -> {
     logger.info(f'get collision_file: {collision_file}')
     logger.info(f'get config_file: {config_file}')
 
-    '''
     config = dataset_reader.Config(config_file)
     collision = dataset_reader.Collision(collision_file)
     log = dataset_reader.Log(log_file)
 
-    metrics = metrics_calculator.calc_metrics(config, log, collision)
-    score = metrics_calculator.score_of_metrics(metrics)
+    result = metrics_calculator.calc_metrics(config, log, collision)
+    score = metrics_calculator.score_of_metrics(result)
+    result['score'] = int(score)
+    return result
 
-    TODO: metrics is a dict, score is an int
-    '''
 
-    return {
-        'jerk': 5117,
-        'velo': 5117,
-        'yaw2lane': 5117,
-        'collision': 0,
-        'duration': 2620
-    }
+def main():
+    scenario_id = sys.argv[1]
+    simulation_logs_dir = sys.argv[2]
+    logger = logging.getLogger("metrics")
+    # find all all log files under simulation_logs_dir, which name like scenario{scenario_id}*.txt
+    log_files = metrics_utils.get_log_files(scenario_id, simulation_logs_dir)
+    metrics = do_metric(logger, log_files)
+    logger.info(metrics)
 
 
 if __name__ == "__main__":
-    scenario_id = sys.argv[1]
-    simulation_logs_dir = sys.argv[2]
-
-    # find all all log files under simulation_logs_dir, which name like scenario{scenario_id}*.txt
-    log_files = metrics_utils.get_log_files(scenario_id, simulation_logs_dir)
-    do_metric(logging.getLogger("metrics"), log_files)
+    main()
